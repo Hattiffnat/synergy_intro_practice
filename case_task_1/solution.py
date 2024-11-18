967cb0cf9febeb56fb0b28bdcb667852d249ed91
My Name <my@email.com>
2024-11-18 22:40:35 +0300

    добавил задачи 4 и 5

--- case_task_1/solution.py
+++ case_task_1/solution.py
@@ -1,10 +1,25 @@
 from typing import Iterable
+import unittest
 
 
-def solution(arr: Iterable[float | int]) -> float | int:
+def sum_neg_between_first_and_last(arr: Iterable[float | int]) -> float | int:
     return sum(x for x in sorted(arr)[1:-1] if x < 0)
 
 
+def main():
+    print(
+        "Сумма отрицательных элементов, расположенных между максимальным и минимальным:",
+        sum_neg_between_first_and_last(
+            map(float, input("Введите числа через пробел >> ").split())
+        ),
+    )
+
+
 if __name__ == "__main__":
-    assert solution([1, -2, 3, -6, 4]) == -2
-    assert solution([1, -2, 3, -6, 4, -100]) == -8
+    main()
+
+
+class TestSolution(unittest.TestCase):
+    def test_sum_neg(self):
+        assert sum_neg_between_first_and_last([1, -2, 3, -6, 4]) == -2
+        assert sum_neg_between_first_and_last([1, -2, 3, -6, 4, -100]) == -8
