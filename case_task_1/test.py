47d2909c85cd9ac0f51dca5012e5f39763359fd4
Hattiffnat <sereyfeam@gmail.com>
2025-02-10 12:34:38 +0300

    убрал пробел

--- case_task_1/test.py
+++ case_task_1/test.py
@@ -5,7 +5,7 @@ from case_task_1.solution import sum_neg_between_first_and_last
 class TestSolution(unittest.TestCase):
     def test_sum_neg(self):
         self.assertEqual(sum_neg_between_first_and_last([1, -2, 3, -6, 4]), -2)
-        self.assertEqual( sum_neg_between_first_and_last([1, -2, 3, -6, 4, -100]), -8)
+        self.assertEqual(sum_neg_between_first_and_last([1, -2, 3, -6, 4, -100]), -8)
         self.assertEqual(sum_neg_between_first_and_last([]), 0)
         self.assertEqual(sum_neg_between_first_and_last([135, 13, 22, 355, 989]), 0)
         self.assertEqual(sum_neg_between_first_and_last([-135, -13, -22, -355, -989]), -512)
